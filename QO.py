3a59c7e65446fef4533892179dddac4c65ead4d4
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2021-07-15 11:54:49 +0300

    Update QO.py

--- QO.py
+++ QO.py
@@ -2,26 +2,31 @@ print("Interacting with reality...")
 import numpy as np
 
 class Photon(object):
-    def __init__(self,λ,location,direction):
-        self.c=299792458#m/s
+    def __init__(self,λ,location,direction,spin=(1,0,0),polarization=(1,0)):
+        self.__c=299792458#m/s
 
-        self.location=np.array(location,dtype=np.float32)
-        self.direction=np.array(direction,dtype=np.float32)
-        if self.location.shape not in ((),(2,),(3,)):
+        self.__location=np.array(location,dtype=np.float32)
+        self.__direction=np.array(direction,dtype=np.float32)
+        self.__direction=self.__direction/np.linalg.norm(self.__direction)
+        if self.__location.shape not in ((),(2,),(3,)):
             raise ValueError("Photons only be allocated in 1D, 2D and 3D")
-        if self.direction.shape not in ((),(2,),(3,)):
+        if self.__direction.shape not in ((),(2,),(3,)):
             raise ValueError("Photons only be pointed in 1D, 2D and 3D")
-        self.lambdaa=λ
+        if not self.__direction.shape==self.__location.shape:
+            raise ValueError("Position and direction vectors must be in the same dimensions")
+        self.__lambdaa=λ
 
     def evolve_time(self,seconds):
-        co=self.c/np.linalg.norm(self.direction)
-        self.location+=self.direction*co*seconds
+        self.__location+=self.__direction*self.__c*seconds
     
-    def get_location(self):
-        return self.location
+    def info(self):
+        return {"wavelenght":self.__lambdaa,"location":self.__location,"direction":self.__direction}
     
-    def get_direction(self):
-        return self.location
-        
-    def get_wavelenght(self):
-        return self.lambdaa
+class Beam(object):
+    def __init__(self,Photons):
+        self.__photons=np.array(Photons)
+        if not type(Photons) in (tuple,list):
+            raise TypeError("Photons must be in a tuple or list")
+
+    def info(self):
+        return self.__photons
