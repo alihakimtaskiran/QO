a26fa383486f2eab6b3a8c1aa55f83f6eb7f8375
Ali Hakim Taşkıran <52009742+alihakimtaskiran@users.noreply.github.com>
2022-03-10 16:46:39 +0300

    Update QO.py

--- QO.py
+++ QO.py
@@ -1,61 +1,72 @@
 print("Interacting with reality...")
 import os
+import numpy as np
 
-class PhotonField(object):
+os.system("mkdir MemoriesWillNeverFade") #For the memory of survey of learning photonics. Appreciate all inspiring me
+
+class Field(object):
     def __init__(self,dim):
+        if not type(dim)==int:
+            raise TypeError("Number of dimensions is an integer")
+        if dim<1:
+            raise ValueError("Number of dimensions must be more than 1")
         self.__dim=dim
-        self.__Φ=""
-        self.c=299792458#m/s
-    
-    def shine(self,location,presence,amplitude, frequency, phase=0):
-        if not(type(location)==tuple or type(location)==list):
-            raise TypeError("Location of the light must be a list or tuple")
-        location=tuple(location)
-        if not( type(presence)==tuple or type(presence)==list):
-            raise TypeError("Presence of the light must be a list or tuple")
-        if not type(amplitude)==float:
-            raise TypeError("Amplitude of the light must be real scalar")
-        if not( type(frequency)==float or type(frequency)==int):
-            raise TypeError("Frequency of the light must be real scalar")
-        if not( type(phase)==float or type(phase)==int):
-            raise TypeError("phase of the light must be real scalar")
-        d=len(location)
-        if not d==self.__dim:
-            raise ValueError("Location must have the same dimensions with the entity")
-        for i in range(d):
-            self.__Φ+=str(location[i])+","
-        self.__Φ+=str(presence[0])+","
-        self.__Φ+=str(presence[1])+","
-        self.__Φ+=str(amplitude)+","
-        self.__Φ+=str(frequency)+","
-        self.__Φ+=str(phase)+"\n"
+        self.__shinies=[]
         
-     
-       
-    def observeC(self,location,moment):
-        if not(type(location)==tuple or type(location)==list):
-            raise TypeError("Location of the light must be a list or tuple")
-        if not (type(moment)==float or type(moment)==int):
-            raise TypeError("the moment is a scalar")
-        r_val=0
-        file=open("result.cw","w")
-        file.write("")
-        file.close()
-        file=open("wave.cw","w")
-        file.write(self.__Φ)
-        file.close()
-        file=open("observer.cw","w")
-        _=""
-        _+=str(moment)
-        _+=","+str(moment)
-        for i in range(self.__dim):
-            _+=","+str(location[i])
+    def add(self, content):
+        if not type(content) in {list, tuple}:
+            content = (content,)
         
+        for sub_content in content:
+            if type(sub_content)==Shiny:
+                _=len(sub_content.info[0])
+                if not _==self.__dim:
+                    raise ValueError(f"The source must be located in {self.__dim}-D space. But, it is located in {_}-D space")
+                self.__shinies.append(sub_content)
+
+    def observe(self, locations, moment):
+        locations=np.array(locations)
+        if not locations.shape==(self.__dim,):
+            if not locations.shape[1]==self.__dim:
+                raise ValueError("Observer's locations must be N×D array. N is number of observers D is dimensions of the entity")
+                
+        else:
+            locations=np.array((locations,))
+            
+        np.savetxt("MemoriesWillNeverFade/observers.wm",locations , delimiter=",")  
+        files=(open("MemoriesWillNeverFade/meta.wm","w"),open("MemoriesWillNeverFade/locations.wm","w"),open("MemoriesWillNeverFade/amplitudes.wm","w"),open("MemoriesWillNeverFade/frequencies.wm","w"),open("MemoriesWillNeverFade/phases.wm","w"),open("MemoriesWillNeverFade/result.wm","w"))
+        files[0].write(str(self.__dim)+"\n"+str(len(self.__shinies))+"\n"+str(moment)+"\n"+str(locations.shape[0]))
+        for glimmer in self.__shinies:
+            if glimmer.info[1][0]<=moment<=glimmer.info[1][1]:
+                files[1].write(str(glimmer.info[0])[1:-1]+"\n")
+                files[2].write(str(glimmer.info[2])+"\n")
+                files[3].write(str(glimmer.info[3])+"\n")
+                files[4].write(str(glimmer.info[4])+"\n")
         
-        file.write(_)
-        file.close()
-        os.system("./renderer.bin")
-        file=open("result.cw")
-        r_val=float(file.read())
-        file.close()
-        return r_val   
+        for file in files:
+            file.close()
+        
+        os.system("./renderer")
+        return np.genfromtxt("MemoriesWillNeverFade/result.wm",delimiter=";")
+        
+            
+
+
+class Shiny(object):
+    def __init__(self, location, presence, amplitude, frequency, phase=0):
+        if type(location) not in {list, tuple}:
+            raise TypeError("Location must be a list or tuple")
+
+        if type(presence) not in {list, tuple}:
+            raise TypeError("Presence must be a list or tuple")
+
+        if type(amplitude) not in {int, float}:
+            raise TypeError("Amplitude must be a int or float")       
+
+        if type(frequency) not in {int, float}:
+            raise TypeError("Frequency must be a int or float")
+
+        if type(phase) not in {int, float}:
+            raise TypeError("Phase must be a int or float")
+
+        self.info=(location, presence, amplitude, frequency, phase)
